de2d652fa20b8236ee1bd53aa5cb3cfcf95768d5
Lyle Franklin <lfranklin@pivotal.io>
2017-04-18 15:11:43 -0700

    Move ruby bosh CLI to bosh1, install bosh from tap
    [#143843605]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- capi/recipes/bosh.rb
+++ capi/recipes/bosh.rb
@@ -20,3 +20,22 @@ end
 
 chruby_gem('bundler')
 chruby_gem('bosh_cli')
+
+# rename old BOSH CLI to bosh1
+rubies = node['sprout']['chruby']['rubies']
+rubies.each do |_, ruby_versions|
+  ruby_versions.each do |ruby_version|
+    execute "renaming ruby bosh CLI to bosh1 for ruby #{ruby_version}" do
+      old_bosh_path = "#{node['sprout']['home']}/.gem/ruby/#{ruby_version}/bin/bosh"
+      command "mv #{old_bosh_path} #{old_bosh_path}1"
+      user node['sprout']['user']
+      only_if "test -f #{old_bosh_path}"
+    end
+  end
+end
+
+homebrew_package 'bosh-cli' do
+  homebrew_user node['sprout']['user']
+  action :upgrade
+  options '--without-bosh2'
+end
