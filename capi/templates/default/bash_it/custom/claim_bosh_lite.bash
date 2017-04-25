e963724161048656ca092063adf1189f4b102fad
Matt Royal <mroyal@pivotal.io>
2017-04-25 14:13:20 -0700

    Add claim_bosh_lite and unclaim_bosh_lite functions to bash
    These claim/unclaim a remote bosh lite environment from our pool and set up the env vars for the bosh cli
    [#143300605]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

---
+++ capi/templates/default/bash_it/custom/claim_bosh_lite.bash
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+function claim_bosh_lite() {
+  function msg {
+    echo -e $1
+  }
+
+  function realpath {
+    echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1")
+  }
+
+  function claim_random_environment() {
+    pool="bosh-lites"
+
+    for f in ./${pool}/unclaimed/*; do
+      test -f "$f" || continue
+
+      msg "Claiming $( basename $f )..."
+      claim_specific_environment $(basename $f)
+      return $?
+    done
+
+    msg "No unclaimed environment found in $pool"
+    return 1
+  }
+
+  function claim_specific_environment() {
+    env=$1
+
+    git pull --rebase --quiet
+
+    file=`find . -name $env`
+
+    if [ "$file" == "" ]; then
+      echo $env does not exist
+      return 1
+    fi
+
+    set +e
+    file_unclaimed=`echo $file | grep claim | grep -v unclaim`
+    set -e
+
+    if [ $file_unclaimed ]; then
+      msg $env could not be claimed
+      return 1
+    fi
+
+    newfile=`echo ${file} | sed -e 's/unclaimed/claimed/'`
+
+    git mv $file $newfile
+    git ci --quiet --message "manually claim ${env} on ${HOSTNAME} [nostory]" --no-verify
+    msg "Pushing reservation to $( basename $PWD )..."
+    git push --quiet
+    msg "Done\n"
+  }
+
+  env_file=$(
+    set -e
+    >&2 cd ~/workspace/capi-ci-private
+    >&2 claim_random_environment $requested_input
+
+    realpath $newfile
+  )
+
+  if [ "$?" == 0 ]; then
+    source "${env_file}"
+
+    env_name="$( basename "${env_file}" )"
+    msg "Now targeting Director at \`${BOSH_ENVIRONMENT}\`"
+    msg "Your CF system_domain is \`${BOSH_LITE_DOMAIN}\`"
+    msg "To target that environment in other sessions, run \`source $( realpath ${env_file} )\`"
+    msg "To unclaim the environment, run \`unclaim_bosh_lite ${env_name}\`"
+  fi
+}
+
+export -f claim_bosh_lite
---
+++ capi/templates/default/bash_it/custom/unclaim_bosh_lite.bash
@@ -0,0 +1,51 @@
+function unclaim_bosh_lite() {
+  (
+    cd ~/workspace/capi-ci-private
+
+    if [ $# -eq 0 ]; then
+      echo 'Usage: $0 env_name'
+      return 1
+    fi
+
+    git pull -r --quiet
+
+    function unclaim {
+      env=$1
+      file=`find . -name $env`
+
+      if [ "$file" == "" ]; then
+        echo $env does not exist
+        return 1
+      fi
+
+      set +e
+      file_unclaimed=`echo $file | grep unclaim`
+      set -e
+
+      if [ $file_unclaimed ]; then
+        echo $env is not claimed
+        return 1
+      fi
+
+      read -p "Hit enter to unclaim $env "
+
+      newfile=`echo $file | sed -e 's/claimed/unclaimed/'`
+      git mv $file $newfile
+      git ci -m"manually unclaim $env on ${HOSTNAME} [nostory]" --no-verify
+      echo "Pushing the unclaim commit to $( basename $PWD )..."
+      git push --quiet
+    }
+
+    for env in "$@"; do
+      unclaim $env
+    done
+
+  )
+
+  unset BOSH_CA_CERT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_ENVIRONMENT \
+    BOSH_GW_USER BOSH_GW_HOST BOSH_LITE_DOMAIN BOSH_GW_PRIVATE_KEY_CONTENTS
+
+  echo "Done"
+}
+
+export -f unclaim_bosh_lite
