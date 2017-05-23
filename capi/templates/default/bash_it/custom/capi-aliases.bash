bce64a555851e88cbd2bb19c44e64e68ab1137f5
Lyle Franklin <lfranklin@pivotal.io>
2017-05-23 08:52:56 -0700

    Add alias for uploading dev capi release
    [#139997637]

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -3,16 +3,16 @@ alias resprout='(cd ~/workspace/sprout-capi && git pull && chruby-exec system --
 # CATs
 alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test --nodes=3)'
 
-# Deploying
+# Deploying (bosh1 + cf-release)
 alias qnd-deploy='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite --no-manifest)'
 alias qnd-deploy-diego='(cd ~/workspace/diego-release && bosh --parallel 10 sync blobs && scripts/update && scripts/deploy && bosh deployment ~/workspace/cf-release/bosh-lite/deployments/cf.yml)'
 alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite)'
 
+# Deploying (bosh2 + cf-deployment)
+alias upload-capi-release='(cd ~/workspace/capi-release && bosh sync-blobs && bosh1 create release --force --name capi && bosh upload-release --rebase)'
+
 # PSQL
 alias psql-bosh-lite='psql -h 10.244.0.30 -p 5524 -U ccadmin ccdb'
-
-# XENA
-alias bosh-ssh-xena='(bosh target xena && bosh download manifest cf-xena /tmp/xena.yml && bosh deployment /tmp/xena.yml && bosh ssh --gateway_user vcap --gateway_host bosh.xena.cf-app.com --gateway_identity_file ~/workspace/capi-ci-private/xena/keypair/bosh.pem)'
 
 #FASD
 alias v='fasd -e vim'
