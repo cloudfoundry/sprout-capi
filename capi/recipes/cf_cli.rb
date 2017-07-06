bdf86e1b4983800d56c4254a32314b0f49b11276
Lyle Franklin <lfranklin@pivotal.io>
2017-07-06 09:05:19 -0700

    Remove v3-beta plugin
    We have real commands now!

--- capi/recipes/cf_cli.rb
+++ capi/recipes/cf_cli.rb
@@ -31,11 +31,6 @@ execute 'install cf cli CLI-Recorder plugin' do
   user node['sprout']['user']
 end
 
-execute 'install cf cli v3-cli-plugin plugin' do
-  command "cf uninstall-plugin v3_beta; cf install-plugin https://github.com/cloudfoundry/v3-cli-plugin/releases/download/0.6.7/v3-cli-plugin.osx -f"
-  user node['sprout']['user']
-end
-
 execute 'install cf cli open plugin' do
   command "cf uninstall-plugin open; cf install-plugin Open -r CF-Community -f"
   user node['sprout']['user']
