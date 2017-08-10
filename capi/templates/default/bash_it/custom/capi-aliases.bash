9cd388f025ae5715ea53030b632422033b90e78f
Greg Cobb <gcobb@pivotal.io>
2017-08-10 14:31:39 -0700

    Make all aliases use underscores instead of hyphens
    - refactor creating and deploy as separate scripts
    - [#149203353]
    
    Signed-off-by: Lisa Cho <lcho@pivotal.io>

--- capi/templates/default/bash_it/custom/capi-aliases.bash
+++ capi/templates/default/bash_it/custom/capi-aliases.bash
@@ -4,18 +4,18 @@ alias resprout='(cd ~/workspace/sprout-capi && git pull && chruby-exec system --
 alias cats='(cd ~/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests && CONFIG=$PWD/integration_config.json bin/test --nodes=3)'
 
 # Deploying (bosh1 + cf-release)
-alias qnd-deploy='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite --no-manifest)'
-alias qnd-deploy-diego='(cd ~/workspace/diego-release && bosh --parallel 10 sync blobs && scripts/update && scripts/deploy && bosh deployment ~/workspace/cf-release/bosh-lite/deployments/cf.yml)'
-alias qnd-deploy-manifest='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite)'
+alias qnd_deploy='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite --no-manifest)'
+alias qnd_deploy_diego='(cd ~/workspace/diego-release && bosh --parallel 10 sync blobs && scripts/update && scripts/deploy && bosh deployment ~/workspace/cf-release/bosh-lite/deployments/cf.yml)'
+alias qnd_deploy_manifest='(cd ~/workspace/cf-release && scripts/deploy-dev-release-to-bosh-lite)'
 
 # Deploying (bosh2 + cf-deployment)
-alias upload-capi-release='(cd ~/workspace/capi-release && bosh sync-blobs && bosh create-release --force --name capi && bosh upload-release --rebase)'
-alias create-and-deploy='~/workspace/capi-release/scripts/create-and-deploy'
-alias create-and-force-deploy='~/workspace/capi-release/scripts/create-and-deploy -n'
-alias bootstrap-cf='~/workspace/capi-release/scripts/bootstrap-cf'
+alias upload_capi_release='~/workspace/capi-release/scripts/create-and-upload'
+alias create_and_deploy='upload_capi_release && ~/workspace/capi-release/scripts/deploy'
+alias create_and_force_deploy='upload_capi_release && ~/workspace/capi-release/scripts/deploy -n'
+alias bootstrap_cf='~/workspace/capi-release/scripts/bootstrap-cf'
 
 # PSQL
-alias psql-bosh-lite='~/workspace/capi-release/scripts/psql-bosh-lite'
+alias psql_bosh_lite='~/workspace/capi-release/scripts/psql-bosh-lite'
 
 #FASD
 alias v='fasd -e vim'
