173cd29989815b030ff632cff5986aaf1e8101d6
Greg Cobb <gcobb@pivotal.io>
2017-08-28 17:46:35 -0700

    Clean up sprout-wrap
    - Remove checkman, gnatsd, BitTorrent sync, legacy bosh1 helpers, etc

--- capi/recipes/default.rb
+++ capi/recipes/default.rb
@@ -1,8 +1,5 @@
-include_recipe 'capi::bosh_directors'
 include_recipe 'capi::concourse_cli'
 include_recipe 'capi::spiff'
 include_recipe 'capi::vim_luan'
-include_recipe 'capi::checkman'
 include_recipe 'capi::bosh'
-include_recipe 'capi::gnatsd'
 include_recipe 'capi::bbl_cli'
--- capi/recipes/gnatsd.rb
+++
@@ -1,5 +0,0 @@
-execute 'go get github.com/nats-io/gnatsd' do
-    user node['sprout']['user']
-    environment('GOPATH' => File.join(node['sprout']['home'], 'go'))
-end
-
