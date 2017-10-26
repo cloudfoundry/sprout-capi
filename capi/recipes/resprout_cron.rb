a59d5eb08ebd5d5b019e77a3b878bf5b52490805
Lisa Cho <lcho@pivotal.io>
2017-10-25 17:16:03 -0700

    notify user if soloist fails

--- capi/recipes/resprout_cron.rb
+++ capi/recipes/resprout_cron.rb
@@ -1,5 +1,5 @@
 cron 'resprout-cron' do
   time :daily
   mailto nil
-  command "(cd '#{node['sprout']['home']}/workspace/sprout-capi' && sudo -u #{node['sprout']['user']} git pull && bash -l -c 'cd '#{node['sprout']['home']}/workspace/sprout-capi' && HOME='#{node['sprout']['home']}' SUDO_USER='#{node['sprout']['user']}' launchctl asuser $(id -u #{node['sprout']['user']}) soloist') &> /tmp/sprout.log"
+  command "(trap 'open http://dn.ht/picklecat/' ERR; cd '#{node['sprout']['home']}/workspace/sprout-capi' && sudo -u #{node['sprout']['user']} git pull && bash -l -c 'cd '#{node['sprout']['home']}/workspace/sprout-capi' && HOME='#{node['sprout']['home']}' SUDO_USER='#{node['sprout']['user']}' launchctl asuser $(id -u #{node['sprout']['user']}) soloist') &> /tmp/sprout.log"
 end
