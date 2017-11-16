5b8947f24c25b295b9f308e6ccee9d998c4ba746
Zach Robinson <zrobinson@pivotal.io>
2017-11-16 13:28:25 -0800

    target_bosh no longer horribly destroys terminals

--- capi/templates/default/bash_it/custom/target_bosh.bash
+++ capi/templates/default/bash_it/custom/target_bosh.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 target_bosh() {
-  green='\033[32m'
-  red='\033[31m'
-  nc='\033[0m'
+  # tput setaf 1 = red
+  # tput setaf 2 = green
+  # tput setaf 9 = reset color
 
   pool_dir=~/workspace/capi-env-pool/bosh-lites/claimed
 
@@ -12,14 +12,14 @@ target_bosh() {
   popd >/dev/null
 
   if [ -z "$1" ]; then
-    echo -e "${red}Usage: target_bosh <environment>. Valid environments are:${nc}"
+    echo "$(tput setaf 1)Usage: target_bosh <environment>. Valid environments are:$(tput setaf 9)"
     ls ${pool_dir}
   else
     env_file=${pool_dir}/${1}
 
     if [ -f "$env_file" ]; then
       source "$env_file"
-      echo -e "${green}Success!${nc}"
+      echo "$(tput setaf 2)Success!$(tput setaf 9)"
       env_ssh_key_path="$HOME/workspace/capi-env-pool/${1}/bosh.pem"
 
       if [ ! -f "$env_ssh_key_path" ]; then
@@ -29,7 +29,7 @@ target_bosh() {
 
       export BOSH_GW_PRIVATE_KEY="$env_ssh_key_path"
     else
-      echo -e "${red}Environment '${1}' does not exist. Valid environments are:${nc}"
+      echo "$(tput setaf 1)Environment '${1}' does not exist. Valid environments are:$(tput setaf 9)"
       ls ${pool_dir}
     fi
   fi
