368a2c46b98e54f4a613ab196e22f63c1db0c9f0
Tim Downey <tdowney@pivotal.io>
2017-12-07 15:27:50 -0800

    Remove CATS GOPATH symlink recipe
    - Team has decided to no longer rely on cf-release for its CATS
    
    Signed-off-by: Lisa Cho <lcho@pivotal.io>

--- capi/recipes/cats_gopath_symlink.rb
+++
@@ -1,4 +0,0 @@
-link '/Users/pivotal/go/src/github.com/cloudfoundry/cf-acceptance-tests' do
-  to '/Users/pivotal/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests'
-  only_if 'test -d /Users/pivotal/go && test -d /Users/pivotal/workspace/cf-release/src/github.com/cloudfoundry/cf-acceptance-tests'
-end
